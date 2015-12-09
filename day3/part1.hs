171a7eefad3b62c82a614edf95240f9bd3fcb57f
Ratan <ratan.r.sur@gmail.com>
2015-12-09 14:26:18 -0500

    clean

--- day3/part1.hs
+++ day3/part1.hs
@@ -1,11 +1,5 @@
 import qualified Data.Set as Set
 
-santaPos (x,y) (c:cs)
-    | c == '^' = santaPos (x, y+1) cs
-    | c == 'v' = santaPos (x, y-1) cs
-    | c == '<' = santaPos (x-1, y) cs
-    | c == '>' = santaPos (x+1, y) cs
-
 generateSantaVisited "" = [(0,0)]
 generateSantaVisited (c:cs) = newLoc c $ generateSantaVisited cs
     where
